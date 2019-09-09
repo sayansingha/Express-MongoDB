51720fcec824ad739f49cc1379e682edbfbb6a1f
Sayan Singha <sayansingha@Sayans-MacBook-Pro.local>
2019-09-09 14:16:43 +0530

    Contains updated: 1. The package.json file has been updated with npm start, 2. The server.js has been updated with: 	i. The logs are removed,        ii. The add.delete is used,       iii. The comments are removed.

---
+++ mongo-dal.js
@@ -0,0 +1,70 @@
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+let db
+
+MongoClient.connect(connectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (error, client) => {
+    if (error) {
+        return console.log("Unable to connect to database!")
+    }
+    db = client.db(databaseName)
+})
+
+function read(refId) {
+    let query = {
+        refId: refId
+    }
+    return db.collection('tasks').find(query).toArray().then((result) => {
+        console.log(result)
+        return result
+    })
+}
+
+function del(refId) {
+    let query = {
+        refId: refId
+    }
+
+    return db.collection('tasks').deleteOne(query).then((result) => {
+        return true
+    }).catch((error) => {
+        return false
+    })
+}
+
+function create(refId, content) {
+    return db.collection('tasks').insertOne({
+        refId: refId,
+        content: content
+    }).then((result) => {
+        console.log(result.ops)
+        return result.ops
+    })
+}
+
+function update(refId, content) {
+    return db.collection('tasks').updateOne({
+        refId: refId
+    }, {
+        $set: {
+            content: content
+        }
+    }).then((result) => {
+        console.log(result)
+        return true
+    })
+}
+
+module.exports = {
+    read,
+    del,
+    create,
+    update
+}
